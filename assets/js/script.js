11c915c42a651d0ae58a39f47c4687c76db16d75
CGalko <cash.galko12@ncf.edu>
2021-04-13 21:59:23 -0400

    updated portfolio to include link ro resume, updated screenshots of various projects & gave them titles & links to the deployed pages. This entire portfolio will be overhauled now that I have a better working knowledge of front-end development, but I do not have the time to do so now.

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const weatherDash = document.getElementById('weather-dashboard');
+
+weatherDash.addEventListener('click', function () {
+    window.location.href = 'https://cashgalko.github.io/weather-Dashboard/'
+    
+  });
+
+const cryptoTracker = document.getElementById('crypto-tracker')
+
+cryptoTracker.addEventListener('click', function () {
+    window.location.href = 'https://aboomgaard.github.io/Stonk-Chasers/'
+  });
+
+
+const passwordGen = document.getElementById('passGen')
+
+passwordGen.addEventListener('click', function () {
+    window.location.href = 'https://cashgalko.github.io/Password-Generator/'
+  });
+
