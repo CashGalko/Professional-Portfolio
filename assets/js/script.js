a120fc660e4d9d45eea1c7ef05c0f8b0097043ba
CGalko <cash.galko12@ncf.edu>
2021-05-11 19:03:51 -0400

    Updated profile with new experiences & links to new projects. This portfolio is still frankly in need of a total overhaul once I have more free time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,28 @@ weatherDash.addEventListener('click', function () {
     
   });
 
-const cryptoTracker = document.getElementById('crypto-tracker')
+const cryptoTracker = document.getElementById('crypto-tracker');
 
 cryptoTracker.addEventListener('click', function () {
     window.location.href = 'https://aboomgaard.github.io/Stonk-Chasers/'
   });
 
 
-const passwordGen = document.getElementById('passGen')
+const passwordGen = document.getElementById('passGen');
 
 passwordGen.addEventListener('click', function () {
     window.location.href = 'https://cashgalko.github.io/Password-Generator/'
+  });
+
+const recipe = document.getElementById('recipe-blog');
+
+recipe.addEventListener('click', function () {
+    window.location.href = 'https://app-a-tizer.herokuapp.com/'
+  });
+
+const readMeGen = document.getElementById('markdown')
+
+readMeGen.addEventListener('click', function () {
+    window.location.href = 'https://github.com/CashGalko/Professional-README-Generator'
   });
 
